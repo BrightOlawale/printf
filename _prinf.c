3db46b113439a2bdc6b2ed2bea63b93f9084ea47
root <cynthiaclairechisom@gmail.com>
2022-04-19 10:59:27 -0700

    updated

--- _prinf.c
+++ _prinf.c
@@ -17,5 +17,10 @@ int _printf(const char *format, ...)
 	{
 		return (-1);
 	}
-	va_start(arg_param, format);
+	va_start(arg_param, format)
+		va_start(arg_param, format);
+	 counter = compute(format, options, arg_param);
+	 va_end(arg_param);
+	 return (counter);
+
 }
