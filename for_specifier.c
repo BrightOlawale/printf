181896baff063d1b9c9aab84b88e16f9eff007f2
root <cynthiaclairechisom@gmail.com>
2022-04-19 20:03:07 -0700

    print intergers

--- for_specifier.c
+++ for_specifier.c
@@ -29,3 +29,34 @@ int print_string(va_list arr_list)
 	}
 	return (i);
 }
+
+/**
+ * print_integer - Prints an integer
+ * @arr_list: list of arguments
+ * Return: Will return the amount of characters printed
+ */
+int print_integer(va_list arg_list)
+{
+	int num_length;
+	num_length = print_number(arg_list);
+	return (num_length);
+}
+
+/**
+ * unsigned_integer - Prints Unsigned integers
+ * @list: List of all of the argumets
+ * Return: a count of the numbers
+ */
+int unsigned_integer(va_list arg_list)
+{
+	unsigned int num;
+
+	num = va_arg(arg_list, unsigned int);
+
+	if (num == 0)
+		return (print_unsgined_number(num));
+
+	if (num < 1)
+		return (-1);
+	return (print_unsgined_number(num));
+}
