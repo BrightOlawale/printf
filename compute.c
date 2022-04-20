d4ea6afbe0b4e43931ecae9bbdc762cd495102e9
BrightOlawale <oolatunjibright@yahoo.com>
2022-04-20 09:42:45 +0000

    compute file adjusted

--- compute.c
+++ compute.c
@@ -15,33 +15,37 @@ int compute(const char *format, symbfunc options[], va_list arg_param)
 	{
 		if (format[i] == '%')
 		{
-			for (j = 0; options[j].symb != NULL; j++)
+			if (format[i] == '%' && format[i + 1] == '%')
 			{
-				if (format[i + 1] == options[j].symb[0])
+				_writechar('%');
+				counter++;
+				i++;
+			}
+			else
+			{
+				for (j = 0; options[j].symb != NULL; j++)
 				{
-					char_no = options[j].f(arg_param);
-					if (char_no == -1)
+					if (format[i + 1] == options[j].symb[0])
+					{
+						char_no = options[j].f(arg_param);
+						if (char_no == -1)
+							return (-1);
+						counter += char_no;
+						break;
+					}
+				}
+				if (options[j].symb == NULL && format[i + 1] != ' ')
+				{
+					if (format[i + 1] != '\0')
+					{
+						_writechar(format[i]);
+						counter = counter + 1;
+					}
+					else
 						return (-1);
-					counter += char_no;
-					break;
 				}
+				i++;
 			}
-			if (options[j].symb == NULL && format[i + 1] != ' ')
-			{
-				if (format[i + 1] != '\0')
-				{
-					_writechar(format[i]);
-					counter = counter + 1;
-				}
-				else
-					return (-1);
-			}
-			i++;
-		}
-		if (format[i] == '%' && format[i + 1] == '%')
-		{
-			_writechar('%');
-			counter++;
 		}
 		else
 		{
