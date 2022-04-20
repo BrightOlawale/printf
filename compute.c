3144966fe841d01087f4ea072544dee66a3ddb66
root <cynthiaclairechisom@gmail.com>
2022-04-19 17:08:23 -0700

    descriptions

--- compute.c
+++ compute.c
@@ -3,7 +3,7 @@
  * compute - takes the main string and all the necessary parameters to
  * print a formated string
  * @format: A string containing all the desired characters.
- * @options:  A list of all the posible functions
+ * @options: A list of all the posible functions.
  * @arg_param: A list containing all the argumentents passed to the program.
  * Return: A total count of the characters printed.
  */
