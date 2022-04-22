23f5b40844d340974f0a9acfab029b31c5e3cc6e
BrightOlawale <oolatunjibright@yahoo.com>
2022-04-22 12:34:15 +0000

    compute file worked on

--- compute.c
+++ compute.c
@@ -8,50 +8,44 @@
  */
 int compute(const char *format, symbfunc options[], va_list arg_param)
 {
-	int counter, char_no, i, j;
+	int counter = 0, char_no, i, j;
 
-	counter = 0;
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
-			if (format[i] == '%' && format[i + 1] == '%')
+			for (j = 0; options[j].symb != NULL; j++)
 			{
-				_writechar('%');
-				counter++;
-				i++;
-			}
-			else
-			{
-				for (j = 0; options[j].symb != NULL; j++)
+				if (format[i] == '%' && format[i + 1] == '%')
 				{
-					if (format[i + 1] == options[j].symb[0])
-					{
-						char_no = options[j].f(arg_param);
-						if (char_no == -1)
-							return (-1);
-						counter += char_no;
-						break;
-					}
+					_writechar('%');
+					counter++;
+					break;
 				}
-				if (options[j].symb == NULL && format[i + 1] != ' ')
+				else if (format[i + 1] == options[j].symb[0])
 				{
-					if (format[i + 1] != '\0')
-					{
-						_writechar(format[i]);
-						counter = counter + 1;
-					}
-					else
+					char_no = options[j].f(arg_param);
+					if (char_no == -1)
 						return (-1);
+					counter += char_no;
+					break;
 				}
-				i++;
 			}
+			if (options[j].symb == NULL && format[i + 1] != ' ')
+			{
+				if (format[i + 1] != '\0')
+				{
+					_writechar(format[i]);
+					counter = counter + 1;
+				}
+				else
+					return (-1);
+			}
+			i++;
 		}
 		else
-		{
 			_writechar(format[i]);
 			counter++;
-		}
 	}
 	return (counter);
 }
