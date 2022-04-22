2e0aa92d6a80b89ffbfa2e2686f59e1eb77640e4
root <cynthiaclairechisom@gmail.com>
2022-04-22 04:51:11 -0700

    print_hex

--- b_changes.c
+++ b_changes.c
@@ -140,3 +140,48 @@ int hex_check(int num, char x)
 		return (Hex[num]);
 	return (0);
 }
+
+/**
+ * print_hex - Prints a representation of a decimal number on base16 lowercase
+ * @list: List of the arguments passed to the function
+ * Return: Number of characters printed
+ */
+int print_heX(va_list arr_list)
+{
+	unsigned int num;
+	int len;
+	int rem_num;
+	char *hex_rep;
+	char *rev_hex;
+
+	num = va_arg(arr_list, unsigned int);
+
+	if (num == 0)
+		return (_writechar('0'));
+	if (num < 1)
+		return (-1);
+	len = base_len(num, 16);
+	hex_rep = malloc(sizeof(char) * len + 1);
+	if (hex_rep == NULL)
+		return (-1);
+	for (len = 0; num > 0; len++)
+	{
+		rem_num = num % 16;
+		if (rem_num > 9)
+		{
+			rem_num = hex_check(rem_num, 'X');
+			hex_rep[len] = rem_num;
+		}
+		else
+			hex_rep[len] = rem_num + 48;
+		num = num / 16;
+	}
+	hex_rep[len] = '\0';
+	rev_hex = rev_string(hex_rep);
+	if (rev_hex == NULL)
+		return (-1);
+	_writebase(rev_hex);
+	free(hex_rep);
+	free(rev_hex);
+	return (len);
+}
